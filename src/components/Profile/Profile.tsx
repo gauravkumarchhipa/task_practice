dc2a3f0a64c6b6d6aef0e56b9dc0c4455a8c4e7d
gaurav.chhipa <gaurav.c@zyapaar.com>
2024-06-05 18:27:39 +0530

    image

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,11 +1,12 @@
 import { yupResolver } from '@hookform/resolvers/yup';
-import React from 'react'
+import React, { useState } from 'react'
 import { Controller, useForm, UseFormReturn } from 'react-hook-form';
 import { ProfileValidation } from './ProfileValidation';
 import Select from "react-select";
 import { useRouter } from 'next/router';
 const Profile = () => {
     const router = useRouter();
+    const [imagePreview, setImagePreview] = useState<string | null>(null);
     const { control, handleSubmit, formState: { errors }, reset, watch }: UseFormReturn<any> = useForm<any>({
         resolver: yupResolver(ProfileValidation() as any),
         mode: "onChange",
@@ -13,14 +14,14 @@ const Profile = () => {
     const submitHandler = async (data: any) => {
         createProfileUser(data)
     }
-
+    console.log(errors)
     const createProfileUser = async (data: any) => {
         const response = await fetch('http://localhost:8000/profile', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ firstName: data?.firstName, lastName: data?.lastName, age: data?.age, gender: data?.gender?.value, country: data?.country?.value, city: data?.city?.value })
+            body: JSON.stringify({ firstName: data?.firstName, lastName: data?.lastName, age: data?.age, gender: data?.gender?.value, country: data?.country?.value, city: data?.city?.value, profileImage: imagePreview })
         });
         if (!response.ok) {
             throw new Error('Failed to submit profile data');
@@ -134,6 +135,12 @@ const Profile = () => {
             height: '38px',
         }),
     };
+    const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e?.target?.files?.[0];
+        if (file) {
+            setImagePreview(URL.createObjectURL(file));
+        }
+    };
     return (
         <form className="max-w-sm mx-auto pt-5" onSubmit={handleSubmit(submitHandler)} >
             <div className="mb-6">
@@ -170,6 +177,31 @@ const Profile = () => {
                 {renderErrorMessage(errors?.age)}
             </div>
             <div className="mb-6">
+                <label className="block mb-2 text-sm font-medium text-white" htmlFor="multiple_files">Upload file</label>
+                <Controller
+                    name="profileImage"
+                    control={control}
+                    defaultValue={null}
+                    render={({ field }) => (
+                        <input
+                            className="block w-full text-sm  border rounded-lg cursor-pointer bg-gray-50 text-gray-400 focus:outline-none border-gray-600"
+                            type="file"
+                            accept="image/png, image/jpg, image/jpeg"
+                            onChange={(e: any) => {
+                                handleImageChange(e);
+                                field.onChange(e.target.files[0]);
+                            }}
+                        />
+                    )}
+                />
+            </div>
+            {imagePreview && (
+                <div className="mb-6">
+                    <img src={imagePreview} alt="Profile Preview" className="max-w-xs h-auto" />
+                </div>
+            )}
+
+            <div className="mb-6">
                 <label htmlFor="gender" className="block mb-2 text-sm font-medium text-gray-900">Gender</label>
                 <Controller
                     name="gender"
@@ -180,7 +212,6 @@ const Profile = () => {
                             onChange={(e) => {
                                 field.onChange(e);
                             }}
-                            name="gender"
                             placeholder="Select Gender"
                             options={optionGender}
                             styles={customStyles}
@@ -201,7 +232,6 @@ const Profile = () => {
                                 field.onChange(e);
                             }}
                             isSearchable={true}
-                            name="country"
                             placeholder="Select Country"
                             options={optionCountry}
                             styles={customStyles}
