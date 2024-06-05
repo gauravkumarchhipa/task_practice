541b9ff507f95bfb651959fe22671ad8c535d335
gaurav.chhipa <gaurav.c@zyapaar.com>
2024-06-05 13:56:12 +0530

    login

--- src/components/UiComponent/Header.tsx
+++ src/components/UiComponent/Header.tsx
@@ -35,7 +35,7 @@ const Header = () => {
                                     <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2"
                                         onClick={() => { setLogin(true); }}>
                                         <LoginIcon />
-                                        "Login"
+                                        Login
                                     </button>
                                 }
                             </li>
